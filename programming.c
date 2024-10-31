5faa2715176acb866b125b59a89a936d33edbbed
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-01 00:20:53 +0600

    Array Reverse Using Single Array

--- programming.c
+++ programming.c
@@ -331,37 +331,69 @@
 
 //     return 0;
 // }
-#include<stdio.h>
+// #include<stdio.h>
+
+// int a[100000];
+// int b[100000];
+
+// int main()
+// {
+//     int length;
+
+//     scanf("%d", &length);
+
+//     for(int i = 0; i < length; i++){
+//         scanf("%d", &a[i]);
+//     }
+
+//     for(int i = 0, j = length - 1; i < length; i++, j--){
+//         b[j] = a[i];
+//     }
+
+//     for(int i = 0; i < length; i++){
+//         a[i] = b[i];
+//     }
+
+//     for(int i = 0; i < length; i++){
+//         printf("%d ", a[i]);
+//     }
+//     printf("\n");
+
+//     // for(int i = 0; i < length; i++){
+//     //     printf("%d ", b[i]);
+//     // }
+
+//     return 0;
+// }
+#include <stdio.h>
 
 int a[100000];
-int b[100000];
 
 int main()
 {
-    int length;
+    int n;
 
-    scanf("%d", &length);
+    scanf("%d", &n);
 
-    for(int i = 0; i < length; i++){
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &a[i]);
     }
 
-    for(int i = 0, j = length - 1; i < length; i++, j--){
-        b[j] = a[i];
+    for (int i = 0, j = n - 1; i <= j; i++, j--)
+    {
+
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 
-    for(int i = 0; i < length; i++){
-        a[i] = b[i];
-    }
-
-    for(int i = 0; i < length; i++){
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", a[i]);
     }
-    printf("\n");
 
-    // for(int i = 0; i < length; i++){
-    //     printf("%d ", b[i]);
-    // }
+    printf("\n");
 
     return 0;
 }
