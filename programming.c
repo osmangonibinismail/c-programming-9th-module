1e91f570153df4e69cd91798d35931b22047a67c
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-04 21:31:59 +0600

    array recap

--- programming.c
+++ programming.c
@@ -449,3 +449,36 @@
 
 //     return 0;
 // }
+
+#include <stdio.h>
+
+int a[100000];
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+{
+    scanf("%d", &a[i]);
+}
+
+for (int i = 0, j = n - 1; i <= j; i++, j--)
+{
+
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
